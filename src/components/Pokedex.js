58d1566e8dcbdc4d0d715575bdc085feb5f74a05
Julien Az <julien.azbrg@gmail.com>
2021-03-11 20:08:27 +0100

    fork added isempty

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,24 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Pokemon from './Pokemon';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Pokemon from "./Pokemon";
+import { isEmpty } from "./Utils";
 
 const Pokedex = () => {
-    const [data, setData] = useState([""]);
+  const [data, setData] = useState([""]);
 
-    useEffect(() => {
-        axios
-        .get("https://pokeapi.co/api/v2/pokemon?limit=151")
-        .then((res) =>
-            setData(res.data.results));
-    }, []);
-    return (
-        <section id="pokedex">
-            { data.length > 0 && data.map((pokemon) => (     
-                <Pokemon key={pokemon.id} pokemon={pokemon} />         
-            ))}
-
-        </section>
-    );
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((res) => setData(res.data.results));
+  }, []);
+  return (
+    <section id="pokedex">
+      {!isEmpty(data) &&
+        data.map((pokemon) => <Pokemon key={pokemon.name} pokemon={pokemon} />)}
+    </section>
+  );
 };
 
 export default Pokedex;
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,48 +1,43 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { isEmpty } from "./Utils";
 
 const Pokemon = ({ pokemon }) => {
-    const [data, setData] = useState([""]);
-    // useEffect(() => {
-    //     axios
-    //     .get(pokemon.url)
-    //     .then((res) =>
-    //         setData(res.data));
-    // }, []);
-    fetch(pokemon.url)
-    .then((res) => res.json())
-    .then((data) => {
-        setData(data)
-    })
+  const [data, setData] = useState([""]);
 
-// console.log(data.types['0'].type.name);
-    return (
-        <article className="pokemon">
-            <header className="header-pokemon">
-                <h2>{data.name}</h2>
-                <h4>#{data.id}</h4>
-                <h5>{data.stats[0].base_stat} HP</h5>
-            </header>
-            <div className="img-pokemon">
-                <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/" + data.id + ".svg"} alt=""/>
-            </div>
-            <div className="moves-pokemon">
-                <p className="move-1">
-                    Main Attack : {data.moves['0'].move.name}
-                </p>
-                <p className="move-stat">
-                    {data.stats[1].base_stat} Degats
-                </p>
-            </div>
-            <footer className="footer-pokemon">
-                <p>
-                type : {data.types['0'].type.name} 
-                </p>
-            </footer>
-        </article>
-    );
+  useEffect(() => {
+    axios.get(pokemon.url).then((res) => setData(res.data));
+  }, []);
+  // console.log(data.types['0'].type.name);
+  return (
+    <article className="pokemon">
+      <header className="header-pokemon">
+        <h2>{data.name}</h2>
+        <h4>#{data.id}</h4>
+        <h5>{!isEmpty(data.types) && data.types["0"].type.name} HP</h5>
+      </header>
+      <div className="img-pokemon">
+        <img
+          src={
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/" +
+            data.id +
+            ".svg"
+          }
+          alt=""
+        />
+      </div>
+      <div className="moves-pokemon">
+        <p className="move-1">
+          Main Attack : {!isEmpty(data.types) && data.moves["0"].move.name}
+        </p>
+        <p className="move-stat">
+          {!isEmpty(data.types) && data.stats[1].base_stat} Degats
+        </p>
+      </div>
+      <footer className="footer-pokemon">
+        <p>type : {!isEmpty(data.types) && data.types["0"].type.name}</p>
+      </footer>
+    </article>
+  );
 };
 export default Pokemon;
-
-
-
